acd31ec1648360de14c812adfd2d7041d840cdd3
Up-Min <korealee95@naver.com>
2022-11-03 15:22:53 +0900

    11.3 15:22 test

--- JS/common.js
+++ JS/common.js
@@ -2,15 +2,42 @@ $('.menu_Location').click(function () {
   $('header .location').slideToggle();
 });
 
-$('.nav__menu').mouseover(function () {
-  $('.LoNho').css('opacity', '0');
-  $('.LoYho').css('opacity', '1');
-});
-$('.nav__menu').mouseout(function () {
-  $('.LoNho').css('opacity', '1');
-  $('.LoYho').css('opacity', '0');
-});
-
 $('.menu_Location').click(function () {
   $('header .location').slideToggle();
+});
+
+$('.menu_Location a').mouseover(function () {
+  $('.menu_Location a #LoNho').css('opacity', '0');
+  $('.menu_Location a #LoYho').css('opacity', '1');
+});
+$('.menu_Location a').mouseout(function () {
+  $('.menu_Location a #LoNho').css('opacity', '1');
+  $('.menu_Location a #LoYho').css('opacity', '0');
+});
+
+$('.menu_Brand a').mouseover(function () {
+  $('#LoNho').css('opacity', '0');
+  $('#LoYho').css('opacity', '1');
+});
+$('.menu_Brand a').mouseout(function () {
+  $('#LoNho').css('opacity', '1');
+  $('#LoYho').css('opacity', '0');
+});
+
+$('.menu_Delivery a').mouseover(function () {
+  $('#LoNho').css('opacity', '0');
+  $('#LoYho').css('opacity', '1');
+});
+$('.menu_Delivery a').mouseout(function () {
+  $('#LoNho').css('opacity', '1');
+  $('#LoYho').css('opacity', '0');
+});
+
+$('.menu_Food a').mouseover(function () {
+  $('#LoNho').css('opacity', '0');
+  $('#LoYho').css('opacity', '1');
+});
+$('.menu_Food a').mouseout(function () {
+  $('#LoNho').css('opacity', '1');
+  $('#LoYho').css('opacity', '0');
 });
